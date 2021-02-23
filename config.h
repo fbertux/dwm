a3c246415a8591f0d3a7712d8a41c1f09f2bc6a0
Fabio Berton <fbberton@gmail.com>
2021-02-22 21:56:34 -0300

    config.h: Remove yakyak rule
    I don't use yakyak anymore.
    
    Signed-off-by: Fabio Berton <fbberton@gmail.com>

--- config.h
+++ config.h
@@ -37,7 +37,6 @@ static const Rule rules[] = {
     { "Skype",             NULL,       NULL,       1 << 9,            0,           -1 },
     { "Slack",             NULL,       NULL,       1 << 9,            0,           -1 },
     { "discord",           NULL,       NULL,       1 << 9,            0,           -1 },
-    { "yakyak",            NULL,       NULL,       1 << 9,            0,           -1 },
     { "Emacs",             NULL,       "emacs-chat",1 << 9,           0,           -1 },
     { "xterm-256color",    NULL,       "system",   1,                 0,           -1 },
     { "xterm-256color",    NULL,       "pikachu",  1 << 2,            0,           -1 },
