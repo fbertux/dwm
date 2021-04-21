651226ad2f159f6ade1c329ed50eb425e3495d9b
Fabio Berton <fbberton@gmail.com>
2021-04-21 17:13:58 -0300

    config.h: Add Teams rule
    Signed-off-by: Fabio Berton <fbberton@gmail.com>

--- config.h
+++ config.h
@@ -43,6 +43,7 @@ static const Rule rules[] = {
     { "Slack",             NULL,       NULL,           1 << 9,            0,           -1 },
     { "discord",           NULL,       NULL,           1 << 9,            0,           -1 },
     { "Emacs",             NULL,       "emacs-chat",   1 << 9,            0,           -1 },
+    { "Microsoft Teams - Preview", NULL, NULL,         1 << 9,            0,           -1 },
 };
 
 /* layout(s) */
