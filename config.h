2fb3a94e73c67c3d3d0817b4cc66cd24e171a1b4
Fabio Berton <fbberton@gmail.com>
2021-12-11 10:27:10 -0300

    config.h: Use alacritty as default terminal
    Signed-off-by: Fabio Berton <fbberton@gmail.com>

--- config.h
+++ config.h
@@ -76,7 +76,7 @@ static const Layout layouts[] = {
 /* commands */
         static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_white, "-sb", col_cyan, "-sf", col_white, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 /* Custom commands */
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+3%",     NULL };
